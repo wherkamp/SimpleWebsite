5af5ce173205d7036d9a88a5a751de16d46ef9af
wherkamp <wherkamp@kingtux.me>
2019-10-03 20:55:30 -0400

    Merry Christmas Monology

--- src/main/java/me/kingtux/simplewebsite/SimpleSiteBuilder.java
+++ src/main/java/me/kingtux/simplewebsite/SimpleSiteBuilder.java
@@ -6,13 +6,17 @@ import me.kingtux.javalinvc.JavalinVCBuilder;
 import me.kingtux.javalinvc.WebsiteRules;
 import me.kingtux.javalinvc.WebsiteRulesBuilder;
 import me.kingtux.javalinvc.rg.ResourceGrabber;
+
 import me.kingtux.javalinvc.rg.ResourceGrabbers;
-import me.kingtux.javalinvc.rg.templategrabbers.InternalResourceGrabber;
-import me.kingtux.javalinvc.view.ViewManager;
 import me.kingtux.javalinvc.view.ViewManagerBuilder;
 import me.kingtux.tuxorm.TOConnection;
+import org.apache.commons.lang3.Validate;
 import org.simplejavamail.mailer.Mailer;
+import org.simplejavamail.mailer.config.TransportStrategy;
 
+import static me.kingtux.simplewebsite.SimpleSiteKeys.*;
+
+import java.io.File;
 import java.util.Properties;
 
 public class SimpleSiteBuilder {
@@ -28,10 +32,77 @@ public class SimpleSiteBuilder {
     }
 
 
+    private SimpleSiteBuilder setProperty(String string, String value) {
+        properties.setProperty(string, value);
+        return this;
+    }
+
+    public SimpleSiteBuilder setName(String value) {
+        return setProperty(SITE_NAME, value);
+    }
+
+    public SimpleSiteBuilder setURL(String value) {
+        return setProperty(SITE_URL, value);
+    }
+
+    public SimpleSiteBuilder setViewManagerExtension(String value) {
+        return setProperty(VM_EXTENSION, value);
+    }
+
+    public SimpleSiteBuilder setSitePort(int value) {
+        return setProperty(SITE_PORT, String.valueOf(value));
+    }
+
+    public SimpleSiteBuilder setSSLEnabled(boolean value) {
+        return setProperty(SSL_ENABLED, String.valueOf(value));
+    }
+
+    public SimpleSiteBuilder setSSLFile(File value) {
+        return setProperty(SSL_FILE, value.getAbsolutePath());
+    }
+
+    public SimpleSiteBuilder setSSLPort(int value) {
+        return setProperty(SSL_PORT, String.valueOf(value));
+    }
+
+    public SimpleSiteBuilder setResourceGrabberType(ResourceGrabbers value) {
+        return setProperty(RG_TYPE, value.name());
+    }
+
+    public SimpleSiteBuilder setResourceGrabberPath(String value) {
+        return setProperty(RG_PATH, value);
+    }
+
+    public SimpleSiteBuilder setEmailhost(String value) {
+        return setProperty(EMAIL_HOST, value);
+    }
+
+    public SimpleSiteBuilder setEmailPort(int value) {
+        return setProperty(EMAIL_PORT, String.valueOf(value));
+    }
+
+    public SimpleSiteBuilder setEmailFrom(String value) {
+        return setProperty(EMAIL_FROM, value);
+    }
+
+    public SimpleSiteBuilder setEmailPassword(String value) {
+        return setProperty(EMAIL_HOST, value);
+    }
+
+    public SimpleSiteBuilder setEmailTransportStrategy(TransportStrategy value) {
+        return setProperty(EMAIL_TRANSPORT_STRATEGY, value.name());
+    }
+
+
+    public SimpleSiteBuilder setDatabase(Properties properties) {
+        properties.keySet().forEach(o -> this.properties.setProperty((String) o, properties.getProperty((String) o)));
+        return this;
+    }
+
+    //Build the simple site;
     public SimpleSite create() {
-        Javalin javalin = Utils.createJavalin(properties);
         WebsiteRules rules = WebsiteRulesBuilder.create().setName(properties.getProperty("site.name", "SimpleSite")).setUrl(properties.getProperty("site.url", "{PFFT}")).build();
-        ViewManagerBuilder viewManager = ViewManagerBuilder.create().setExtension(".html").setViewManager("me.kingtux.javalinvc.jtwig.JtwigViewManager");
+        ViewManagerBuilder viewManager = ViewManagerBuilder.create().setExtension(properties.getProperty("vm.extension", ".html")).setViewManager("me.kingtux.javalinvc.jtwig.JtwigViewManager");
         ResourceGrabber resourceGrabber = Utils.getResourceGrabber(properties);
         TOConnection connection = new TOConnection(Utils.createTuxJSQL(properties));
         Mailer mailer = Utils.createMailer(properties);
---
+++ src/main/java/me/kingtux/simplewebsite/SimpleSiteKeys.java
@@ -0,0 +1,20 @@
+package me.kingtux.simplewebsite;
+
+public class SimpleSiteKeys {
+    static final String SITE_NAME = "site.name";
+    static final String SITE_URL = "site.url";
+    static final String VM_EXTENSION = "vm.extension";
+    static final String SITE_PORT = "site.port";
+    static final String SSL_ENABLED = "site.ssl";
+    static final String SSL_FILE = "site.ssl.file";
+    static final String SSL_PORT = "site.ssl.port";
+    static final String RG_TYPE = "rg.type";
+    static final String RG_PATH = "rg.path";
+    static final String EMAIL_HOST = "email.host";
+    static final String EMAIL_PORT = "email.port";
+    static final String EMAIL_FROM = "email.from";
+    static final String EMAIL_PASSWORD = "email.password";
+    static final String EMAIL_TRANSPORT_STRATEGY = "email.ts";
+
+
+}
